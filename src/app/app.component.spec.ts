bdde9af5453666516252766d92782a2253e17e36
Victor Miguez <v_miguez@outlook.es>
2021-09-25 02:37:03 +0200

    Test precommit hook

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,18 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
-import { render, screen } from '@testing-library/angular';
+import { TestBed } from '@angular/core/testing'
+import { RouterTestingModule } from '@angular/router/testing'
+import { AppComponent } from './app.component'
+import { render, screen } from '@testing-library/angular'
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent],
-    }).compileComponents();
-  });
+      declarations: [AppComponent]
+    }).compileComponents()
+  })
   test('should render counter', async () => {
-    await render(AppComponent);
+    await render(AppComponent)
 
-    expect(screen.getByText('hola'));
-  });
-});
+    expect(screen.getByText('hola'))
+  })
+})
